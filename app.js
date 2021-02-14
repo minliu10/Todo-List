79657b38f817299b0d0c062b88d55e18f288b8bb
minliu10 <serendipitous251@gmail.com>
2021-02-14 16:05:15 +0800

    JS Setup 2

--- app.js
+++ app.js
@@ -16,13 +16,32 @@ add.addEventListener("click", e => {
     let todo = document.createElement("div");
     todo.classList.add("todo");
     let text = document.createElement("p");
-    text.classList.add("todo-text")
+    text.classList.add("todo-text");
     text.innerText = todoText;
     let time = document.createElement("p");
     time.classList.add("todo-time");
     time.innerText = todoMonth + " / " + todoDate;
     todo.appendChild(text);
     todo.appendChild(time);
+
+    // create green check and red trash can
+    let completeButton = document.createElement("button");
+    completeButton.classList.add("complete");
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+
+    completeButton.addEventListener("click", e => {
+        let todoItem = e.target.parentElement;
+        todoItem.classList.toggle("done");
+    })
+
+    let trashButton = document.createElement("button");
+    trashButton.classList.add("trash");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+
+    todo.appendChild(completeButton);
+    todo.appendChild(trashButton);
+
+    todo.style.animation = "scaleUp 0.3s forwards";
 
     section.appendChild(todo);
 
