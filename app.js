aa835a273693fbffbfed9bc97d89b0aaf11ad274
minliu10 <serendipitous251@gmail.com>
2021-02-14 18:22:12 +0800

    Merge Sort button

--- app.js
+++ app.js
@@ -153,3 +153,49 @@ if (myList !== null){
     section.appendChild(todo);
     })
 }
+
+function mergeTime(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0; 
+    if (i < arr1.length && j < arr2.length){
+        if (Number(arr1[i].todoMonth) > Number(arr2[j].todoMonth)){
+            result.push(arr2[j]);
+            j++;
+        } else if (Number(arr1[i].todoMonth) < Number(arr2[j].todoMonth)) {
+            result.push(arr1[i]);
+            i++;
+        } else if (Number(arr1[i].todoMonth) == Number(arr2[j].todoMonth)) {
+            if (Number(arr1[i].todoDate) > Number(arr2[j].todoDate)) {
+                result.push(arr2[j])
+                j++;
+            } else {
+                result.push(arr1[i]);
+                i++;
+            }
+        }
+    }
+
+    while ( i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+    return result;
+  }
+
+function mergeSort(arr) {
+    if (arr.length ===1) {
+          return arr;
+    } else {
+        let middle = Math.floor(arr.length / 2);
+        let right = arr.slice(0, middle);
+        let left = arr.slice(middle, arr.length);
+        return mergeTime(mergeSort(right), mergeSort(left));
+    }
+}
+
+console.log(mergeSort(JSON.parse(localStorage.getItem("list"))));
